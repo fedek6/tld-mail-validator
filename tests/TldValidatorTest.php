516ce0a583be82cb682abe2d21379416632be3c8
Konrad Fedorczyk <contact@realhe.ro>
2021-03-16 16:06:33 +0100

    Add propper domains test

--- tests/TldValidatorTest.php
+++ tests/TldValidatorTest.php
@@ -8,10 +8,8 @@ use Fedek6\TldMailValidator\TldValidator;
 use PHPUnit\Framework\TestCase;
 use Fedek6\TldMailValidator\Tests\PhpUnitUtil;
 
-use function PHPUnit\Framework\assertCount;
-
 final class TldValidatorTest extends TestCase
-{   
+{
     private $testFilePath = __DIR__ . '/../data/tlds-alpha-by-domain.txt';
 
     /** @var Fedek6\TldMailValidator\TldValidator $tldValidator  */
@@ -42,5 +40,16 @@ final class TldValidatorTest extends TestCase
     {
         $result = PhpUnitUtil::callMethod($this->tldValidator, 'cleanup', [['row 1', 'row 2', '']]);
         $this->assertCount(1, $result);
+    }
+
+    public function testCheckIfHasDomains(): void
+    {
+        $this->tldValidator->loadTlds($this->testFilePath);
+        $result = $this->tldValidator->getTlds();
+
+        $this->assertTrue(in_array('press', $result, true));
+        $this->assertTrue(in_array('pl', $result, true));
+        $this->assertTrue(in_array('gb', $result, true));
+        $this->assertTrue(in_array('ru', $result, true));
     }
 }
