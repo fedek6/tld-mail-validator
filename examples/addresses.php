e5ccb17c9a913fc1ffc24d7f725037abd07555bf
Konrad Fedorczyk <contact@realhe.ro>
2021-03-17 23:22:09 +0100

    Add example

---
+++ examples/addresses.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'janociapka@onet.pl',
+    'slawomirgierek@azzzaa.plsiaisidid',
+    'mireczekpireczek@zloto.com.pl',
+    'warzyniecpierzyniec@kino.ru',
+    'quasimodo@mail.fr',
+    'sczypiorek.upiorek@gitara.art.pl',
+    'wampir@cool.transylvania',
+    'killall@wampierz.press',
+    'slawojka_mirka@wp.pl',
+];
