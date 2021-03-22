976054908d761648c56497536275d3ae0e2db04a
Konrad Fedorczyk <contact@realhe.ro>
2021-03-22 13:35:43 +0100

    Update TldMailValidator

--- src/TldMailValidator.php
+++ src/TldMailValidator.php
@@ -6,18 +6,29 @@ namespace Fedek6\TldMailValidator;
 
 use Exception;
 use Fedek6\TldMailValidator\TldValidator;
+use Fedek6\TldMailValidator\DnsValidator;
 
 /**
  * Tld mail validator.
  * 
- * @version 1.0.0
+ * @version 1.0.1
  * @author Konrad Fedorczyk <contact@realhe.ro>
  * @package Fedek6\TldMailValidator
  */
 class TldMailValidator
-{
+{   
+    // Which tests should be run
+    const TEST_TLD = 1;
+    const TEST_ALL = 2;
+
     /** @var \Fedek6\TldMailValidator\TldValidator $tldValidator  */
     private $tldValidator;
+
+    /** @var \Fedek6\TldMailValidator\DnsValidator|null $dnsValidator  */
+    private $dnsValidator;
+
+    /** @var int $operationMode */
+    private $operationMode;
 
     /**
      * Constructor.
@@ -25,10 +36,16 @@ class TldMailValidator
      * @return void 
      * @throws Exception 
      */
-    public function __construct(string $storageFilePath)
+    public function __construct(string $storageFilePath, int $testFlag = self::TEST_TLD)
     {
         $this->tldValidator = new TldValidator();
         $this->tldValidator->loadTlds($storageFilePath);
+        $this->operationMode = $testFlag;
+
+        // If nedded create instances of other validators.
+        if ($this->operationMode === self::TEST_ALL) {
+            $this->dnsValidator = new DnsValidator();
+        }
     }
 
     /**
@@ -50,6 +67,18 @@ class TldMailValidator
         $emailParts = explode('@', $email);
         $domain = end($emailParts);
 
-        return $this->tldValidator->checkDomain($domain);
+        if ($this->tldValidator->checkDomain($domain) === false) {
+            return false;
+        }
+
+        // Other tests
+        if (
+            !is_null($this->dnsValidator) 
+            && $this->dnsValidator->checkDomainMx($domain) === false
+        ) {
+            return false;
+        }
+
+        return true;
     }
 }
