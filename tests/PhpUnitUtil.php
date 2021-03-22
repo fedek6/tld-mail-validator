6a91c8848b9aa094b804639db44f406ec9793d64
Konrad Fedorczyk <contact@realhe.ro>
2021-03-22 13:34:56 +0100

    Update test utils

--- tests/PhpUnitUtil.php
+++ tests/PhpUnitUtil.php
@@ -4,10 +4,21 @@ namespace Fedek6\TldMailValidator\Tests;
 
 class PhpUnitUtil
 {
-  public static function callMethod($obj, $name, array $args) {
-        $class = new \ReflectionClass($obj);
-        $method = $class->getMethod($name);
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public static function callMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
         $method->setAccessible(true);
-        return $method->invokeArgs($obj, $args);
+
+        return $method->invokeArgs($object, $parameters);
     }
 }
